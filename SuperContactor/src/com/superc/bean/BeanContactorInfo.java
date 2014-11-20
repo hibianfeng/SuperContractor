43447a3c904aa39e2c22f6a6deca43c2d8772a5c
unknown <hibianfeng@163.com>
2014-11-20 22:58:59 +0800

    Retrive contacts'information from database

--- SuperContactor/src/com/superc/bean/BeanContactorInfo.java
+++ SuperContactor/src/com/superc/bean/BeanContactorInfo.java
@@ -3,16 +3,25 @@ package com.superc.bean;
 public class BeanContactorInfo {
 
 	
-	private String ContactorName;
-	private String ContactorNumber;
+	private String m_ContactorName;
+	private String m_ContactorNumber;
 	
 	public String getContactorName()
 	{
-		return ContactorName;
+		return m_ContactorName;
 	}
 	public String getContactorNumber()
 	{
-		return ContactorNumber;
+		return m_ContactorNumber;
 	}
+	public void setContactorName(String contactorName)
+	{
+		 m_ContactorName = contactorName;
+	}
+	public void setContactorNumber(String contactorNumber)
+	{
+		 m_ContactorNumber=contactorNumber;
+	}
+
 
 }
